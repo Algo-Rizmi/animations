678486b9b9feec86de2a92647e7dbcf8a69012ee
Mohammed Sharafi <37874836+Pr1ntGn0me@users.noreply.github.com>
2023-05-26 20:33:43 +0200

    pong game clear

--- game.js
+++ game.js
@@ -1,23 +1,22 @@
 class Paddle {
-  constructor(posX, posY, width, height, speedX, speedY) {
+  constructor(posX, posY, width, height) {
     this.x = posX;
     this.y = posY;
     this.w = width;
     this.h = height;
-    this.speedX = speedX;
-    this.speedY = speedY;
+    // this.speedX = speedX;
+    // this.speedY = speedY;
   }
 
   update() {
-    this.x += this.speedX;
-    this.y += this.speedY;
-    if (this.x >= 500 || this.x < 0) {
-      this.speedX = this.speedX * -1;
-    }
-
-    if (this.y >= 500 || this.y < 0) {
-      this.speedY = this.speedY * -1;
-    }
+    // this.x += this.speedX;
+    // this.y += this.speedY;
+    // if (this.x >= 500 || this.x < 0) {
+    //   this.speedX = this.speedX * -1;
+    // }
+    // if (this.y >= 500 || this.y < 0) {
+    //   this.speedY = this.speedY * -1;
+    // }
   }
 
   Draw(context) {
@@ -37,8 +36,11 @@ class Ball {
   update() {
     this.x += this.speedX;
     this.y += this.speedY;
-    if (this.x >= 500 || this.x <= 0) {
+    if (this.x >= 500) {
       this.speedX = this.speedX * -1;
+    }
+    if (this.x < -10) {
+      alert("lost the game Sucker!");
     }
     if (this.y >= 500 || this.y <= 0) {
       this.speedY = this.speedY * -1;
@@ -56,8 +58,18 @@ class Ball {
 document.addEventListener("DOMContentLoaded", () => {
   let canvas = document.querySelector("canvas");
   let context = canvas.getContext("2d");
-  let r1 = new Paddle(20, 20, 30, 60, 10, 10);
-  let ball = new Ball(20, 20, 20, 10, 10);
+  let r1 = new Paddle(20, 20, 10, 60);
+  let ballImage = new Image();
+  ballImage.src = "bomb.png";
+  let ball = new Ball(250, 250, 10, 10, 10);
+
+  canvas.addEventListener("mousemove", function (event) {
+    let rect = canvas.getBoundingClientRect();
+    let mouseX = event.clientX - rect.left;
+    let mouseY = event.clientY - rect.top;
+
+    r1.y = mouseY - r1.h / 2;
+  });
 
   function Animate() {
     context.clearRect(0, 0, 500, 500);
@@ -65,6 +77,17 @@ document.addEventListener("DOMContentLoaded", () => {
     //update
     r1.update();
     ball.update();
+
+    // Check collision with the paddle
+    if (
+      ball.x + ball.straal >= r1.x &&
+      ball.x - ball.straal <= r1.x + r1.w &&
+      ball.y + ball.straal >= r1.y &&
+      ball.y - ball.straal <= r1.y + r1.h
+    ) {
+      // Ball has collided with the paddle
+      ball.speedX *= -1; // Reverse the horizontal direction of the ball
+    }
 
     //draw
     r1.Draw(context);
