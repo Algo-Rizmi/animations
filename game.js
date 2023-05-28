b7c07f13ad73d615b9cc0f043eaab36f2b22bb0f
Mohammed Sharafi <37874836+Pr1ntGn0me@users.noreply.github.com>
2023-05-28 14:09:20 +0200

    rare push

--- game.js
+++ game.js
@@ -61,7 +61,7 @@ document.addEventListener("DOMContentLoaded", () => {
   let r1 = new Paddle(20, 20, 10, 60);
   let ballImage = new Image();
   ballImage.src = "bomb.png";
-  let ball = new Ball(250, 250, 10, 10, 10);
+  let ball = new Ball(250, 250, 30, 5, 5);
 
   canvas.addEventListener("mousemove", function (event) {
     let rect = canvas.getBoundingClientRect();
@@ -89,9 +89,17 @@ document.addEventListener("DOMContentLoaded", () => {
       ball.speedX *= -1; // Reverse the horizontal direction of the ball
     }
 
-    //draw
+    //draw paddle
     r1.Draw(context);
-    ball.Draw(context);
+
+    //draw ball
+    context.drawImage(
+      ballImage,
+      ball.x - ball.straal,
+      ball.y - ball.straal,
+      ball.straal * 2,
+      ball.straal * 2
+    );
 
     //loop again
     setTimeout(Animate, 33);
@@ -99,3 +107,25 @@ document.addEventListener("DOMContentLoaded", () => {
 
   Animate();
 });
+
+function capitalWord(str) {
+  let text = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === " ") {
+      text += " ";
+    } else if (i % 2 === 1) {
+      text += str[i].toUpperCase();
+    } else {
+      text += str[i];
+    }
+  }
+  return text;
+}
+
+console.log(capitalWord("javascripte"));
+console.log(
+  capitalWord(
+    "they fought for our freedom and we will never forget their service"
+  )
+);
